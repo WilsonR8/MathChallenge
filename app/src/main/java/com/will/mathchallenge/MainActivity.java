6fd126f16e4fde64d2fd22086d4aa0ea3e4727b3
Wilson <wilsonst.suarez@hotmail.com>
2020-08-28 22:32:11 -0500

    Corrections

--- app/src/main/java/com/will/mathchallenge/MainActivity.java
+++ app/src/main/java/com/will/mathchallenge/MainActivity.java
@@ -12,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView pregunta;
     private EditText respuesta;
     private TextView responder;
+    private TextView puntajetxt;
     private TextView puntaje;
     private Pregunta preguntita;
     private String convert;
+    private int p=0;
+    private String pnew;
 
 
     @Override
@@ -24,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Referenciar
-        puntaje= findViewById(R.id.puntajetxt);
+        puntajetxt= findViewById(R.id.puntajetxt);
+        puntaje= findViewById(R.id.puntaje);
         responder= findViewById(R.id.responder);
         respuesta= findViewById(R.id.respuesta);
         pregunta= findViewById(R.id.pregunta);
@@ -32,9 +36,12 @@ public class MainActivity extends AppCompatActivity {
         preguntita= new Pregunta();
 
 
+
         preguntita.numeros();
         pregunta.setText(""+preguntita.getA() + " " + "+"+" "+preguntita.getB());
         convert = Integer.toString(preguntita.getC());
+        pnew= Integer.toString(p);
+
 
 
 
@@ -45,11 +52,16 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(this,test+"",Toast.LENGTH_LONG).show();
 Log.e("intento",""+convert);
             Log.e("averlan","hola"+ test);
+            Log.e("ss","inss"+ pnew);
 
 
      if(convert.equals(test)){
             //preguntita.numeros();
-            pregunta.setText("Wenas");
+         p++;
+            puntaje.setText(""+p);
+
+         preguntita.numeros();
+         pregunta.setText(""+preguntita.getA() + " " + "+"+" "+preguntita.getB());
         }
 
         });
